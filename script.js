65cade1fdbfc1d02824780b06af021f46876cfab
Zanhua1999 <skyair886@gmail.com>
2020-02-13 14:53:30 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,20 @@
+var btn = document.getElementById("btn");
+
+
+btn.onclick = function(){
+
+    var input = document.getElementById("typen");
+    value = input.value;
+    document.querySelector('body').style.background=value;
+    
+}
+
+
+
+
+
+
+
+
+
+
